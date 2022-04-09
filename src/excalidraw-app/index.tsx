104fbebedd26049b39a5a20ba06f43c9cf2d89af
nigo81 <tujiabing81@163.com>
2022-04-09 10:54:02 +0800

    delete introduction

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -270,19 +270,7 @@ const initializeScene = async (opts: {
   return { scene: null, isExternalScene: false };
 };
 
-const PlusLinkJSX = (
-  <p style={{ direction: "ltr", unicodeBidi: "embed" }}>
-    Introducing Excalidraw+
-    <br />
-    <a
-      href="https://plus.excalidraw.com/plus?utm_source=excalidraw&utm_medium=banner&utm_campaign=launch"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Try out now!
-    </a>
-  </p>
-);
+const PlusLinkJSX = "";
 
 const ExcalidrawWrapper = () => {
   const [errorMessage, setErrorMessage] = useState("");
